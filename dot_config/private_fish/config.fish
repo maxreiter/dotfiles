3943893a6cbbe3a92d202427f8917def1bb79d73
Max Reiter <max.reiter@phoenixcollege.edu>
2025-03-07 06:44:25 -0700

    fish: launch sway automatically when we're in a login shell

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -13,3 +13,10 @@ alias task go-task
 
 # ABBREVATIONS
 abbr -a -- cz 'chezmoi'
+
+# Start sway if we're in a login shell
+if status is-login
+	if test -z "$WAYLAND_DISPLAY" -a \( -n "$XDG_VTNR" \) -a \( "$XDG_VTNR" -eq 1 \)
+		exec sway > .local/state/sway.log 2>&1
+	end
+end
